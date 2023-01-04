4d455f0a0dda9410142e1b62bf06bcc5b38bad6c
James Barker <jbarkerlouth@gmail.com>
2023-01-04 23:24:06 +0100

    added the cv controller and CV to index page

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :cv, only: %i[index]
+  resources :contacts, only: %i[index]
+
 end
